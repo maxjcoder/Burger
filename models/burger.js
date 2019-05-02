fec17ac874247f0403610b54e867782c76e7284a
team awesome <maxj@email.arizona.edu>
2019-05-02 16:59:19 -0700

    added update method to orm.js & update funct to burgers.js

--- models/burger.js
+++ models/burger.js
@@ -1,10 +1,14 @@
 var orm = require('../config/orm.js');
 
 var burger = {
-    all: function(cb){
-        orm.all('burgers', function(res){
+    all: function (cb) {
+        orm.all('burgers', function (res) {
             cb(res);
         })
+    },
+
+    update: function (id, cb) {
+        orm.update('burgers', id, cb);
     }
 }
 
