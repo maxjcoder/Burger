e9fe9af6d9a31e976232688835425ced23bb9764
team awesome <maxj@email.arizona.edu>
2019-05-05 10:39:29 -0700

    HTML & CSS updates & process.env.JAWSDB_URL added

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,15 @@
 var mysql = require("mysql");
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'Ginger-Beer-Axe-9',
     database: 'burgers_db'
-})
+});
+};
 
 connection.connect(function (err) {
     if (err) throw err;
