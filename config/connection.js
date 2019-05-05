ac124d869cd9e6132c4a25dc7f85e5888bb4d7c1
team awesome <maxj@email.arizona.edu>
2019-05-05 11:02:16 -0700

    heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -3,12 +3,12 @@ var mysql = require("mysql");
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Ginger-Beer-Axe-9',
-    database: 'burgers_db'
-});
+    var connection = mysql.createConnection ({
+        host: 'localhost',
+        user: 'root',
+        password: 'Ginger-Beer-Axe-9',
+        database: 'burgers_db'
+    });
 };
 
 connection.connect(function (err) {
